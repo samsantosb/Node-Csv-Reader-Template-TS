eb5f146eed0e6f8aefeb7c8392fa09614caeeeb2
samsantosb <samsantosb@outlook.com>
2022-09-20 15:47:14 -0300

    id: new id-parser

--- src/repositories/mongo.repository.ts
+++ src/repositories/mongo.repository.ts
@@ -1,12 +1,12 @@
-import { Db } from "mongodb";
+import { Db, ObjectId } from "mongodb";
 
 export class MongoRepository {
   constructor(private readonly db: Db) {}
   async getAll(collection: string) {
     return this.db.collection(collection).find().toArray();
   }
-  async getOne(collection: string, id: string) {
-    return this.db.collection(collection).find({ _id: id }).toArray();
+  async getOne(collection, id) {
+    return this.db.collection(collection).findOne({ _id: new ObjectId(id) });
   }
   async create(collection: string, data: any) {
     return this.db.collection(collection).insertOne(data);
